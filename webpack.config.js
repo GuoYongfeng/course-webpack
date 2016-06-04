cdd4ce240b34125c6dd7ea0a16bf0e5f52246d11
guoyff <guoyff@yonyou.com>
2016-06-04 16:05:03 +0800

    Save

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+  	loaders: [
+  		{
+  			test: /\.js$/,
+  			loader: 'babel'
+  		}
+  	]
+  }
+};
