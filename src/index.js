09d07d2c8d5289412c83a4c658249f4a347becac
guoyff <guoyff@yonyou.com>
2016-06-04 16:09:32 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+class MyComponent extends Component {
+	render(){
+		return <h1>欢迎参加珠峰React课程</h1>
+	}
+}
+
+let rootEle = document.getElementById('app')
+
+render(<MyComponent />, rootEle)
